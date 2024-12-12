1563bbdfd150a005d48ceddfc9eee95e96792983
Eloiza <eloiza.souza@zup.com.br>
2024-12-12 11:22:50 -0300

    feat: implements method to show difficulty menu

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,22 @@ public class Main {
 
     //Guess Game
     private static void guessGame(Scanner scanner) {
+        boolean running = true;
+        int choice = 0;
+
+        System.out.println("\n* * * * * J O G O   D O   A D I V I N H A * * * * *\n");
+        System.out.println("Bem vindo ao Jogo!\n\nA qualquer momento que deseje encerrar o jogo e sair digite '0' (zero)");
+
 
     }
 
+    //Display menu with the difficulty options
+    private static void showDifficultyMenu() {
+        System.out.println("Escolha o nível de dificuldade desejado: ");
+        System.out.println("""
+                1 - Fácil
+                2 - Médio
+                3 - Difícil""");
+        System.out.print("Opção: ");
+    }
 }
