e662259cf80c5a5d0ccfab386f867ac736a96516
Eloiza <eloiza.souza@zup.com.br>
2024-12-12 11:24:14 -0300

    feat: implements method read from keyboard a integer number

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,12 @@ public class Main {
         System.out.println("\n* * * * * J O G O   D O   A D I V I N H A * * * * *\n");
         System.out.println("Bem vindo ao Jogo!\n\nA qualquer momento que deseje encerrar o jogo e sair digite '0' (zero)");
 
+        do {
+            showDifficultyMenu();
+            choice = readInteger(scanner);
+            if (choice == 0)
+                return;
+        } while (choice < 0 || choice > 3);
 
     }
 
@@ -28,5 +34,15 @@ public class Main {
                 2 - Médio
                 3 - Difícil""");
         System.out.print("Opção: ");
+    }
+
+    //Read from keyboard a integer number
+    private static int readInteger(Scanner scanner) {
+        try {
+            return scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Número inválido. Tente novamente");
+            return -1;
+        }
     }
 }
