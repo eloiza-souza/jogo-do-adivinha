7271c881a913f478b559d0158d986ba893558c52
Eloiza <eloiza.souza@zup.com.br>
2024-12-12 09:50:09 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void guessGame() {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        System.out.println("* * * J O G O   D O   A D I V I N H A * * *");
+
+        while (running) {
+
+            running = false;
+        }
+        System.out.println(" ");
+        scanner.close();
+    }
+
+
+
+
+}
+
